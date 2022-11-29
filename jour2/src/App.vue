73599a0779be098e064e7278ffb888e8f8b006bf
agomesdacosta3 <caps92@live.fr>
2022-11-29 11:15:57 +0100

    suite jour 1 et jour 2

---
+++ jour2/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Premier from "./components/Premier.vue"
+import Deuxieme from './components/Deuxieme.vue'
+import Troisieme from './components/Troisieme.vue';
+import Quatrieme from './components/Quatrieme.vue';
+
+</script>
+
+<template>
+
+  <Premier />
+  <Deuxieme />
+  <Troisieme />
+  <Quatrieme />
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
