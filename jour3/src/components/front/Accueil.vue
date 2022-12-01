a6eddc11164ebb7ddbeab9ec88bef8f7e2994569
agomesdacosta3 <caps92@live.fr>
2022-12-01 09:23:02 +0100

    suite jour 3 et début jour 4

--- jour3/src/components/front/Accueil.vue
+++ jour3/src/components/front/Accueil.vue
@@ -1,13 +1,72 @@
 <template>
     <div>
-        <h1>Bienvenue à l'accueil</h1>
-
-        
+        <button class="btn btn-success" @click="tachesStore.nouvelleTache()">ajouter une nouvelle tâche</button>
+        <h1>{{ tachesStore.titre }}</h1> 
+        <!-- afficher la liste de tâches -->
+        <div class="row">
+            <article class="col-4" v-for="todo , index in tachesStore.taches" :key="index">
+                <div class="card">
+                    <header class="card-header">
+                        <h2>{{ todo.nom }}</h2>
+                    </header>  
+                </div>
+            </article>
+        </div>
+        <h2>tâche en cours </h2>
+        <div class="row">
+            <article class="col-4" v-for="todo , index in tachesStore.encours" :key="index">
+                <div class="card">
+                    <header class="card-header">
+                        <h2>{{ todo.nom }}</h2>
+                    </header>  
+                </div>
+            </article>
+        </div>
+       <!--  {{ JSON.stringify( tachesStore.taches ) }} -->
+        <h1>Bienvenu</h1>
+        <!-- <pre>
+            {{ JSON.stringify(articles , null , " ") }}
+        </pre> -->
+        <div class="row">
+            <article v-for="article , index in articles" :key="index" class="col-3">
+                <h2 v-more="article.title"></h2>
+                <p v-more.body="article.body"></p>
+                <RouterLink :to="{name : 'single' , params : {id : article.id}}" class="btn btn-primary">lire la suite ...</RouterLink> 
+            </article>
+        </div>
     </div>
 </template>
-
-<script>
-    export default {
-        
-    }
+<script setup>
+// équivalent avec API Composition 
+import { ref , onMounted } from "vue"
+import { RouterLink } from "vue-router"
+import { useTodoStore } from "../../stores/todoStore"
+let articles = ref([]);
+let tachesStore = useTodoStore() ;
+//let articles = reactive([]);
+onMounted( () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(reponse => reponse.json())
+            .then(data => articles.value = data)
+} )
 </script>
+
+<!-- <script>
+// api options
+    import { RouterLink } from "vue-router"
+    export default {
+        data : () => {
+            return {
+                articles : []
+            }
+        },
+        components : {
+            "RouterLink" : RouterLink
+        },  
+        mounted : function(){
+            fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(reponse => reponse.json())
+            .then(data => this.articles = data)
+        }
+    }
+</script> -->
--- jour3/src/main.js
+++ jour3/src/main.js
@@ -2,8 +2,19 @@ import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
 import router from "./router"; 
+import { createPinia } from "pinia" ; // <Provider>
 
-createApp(App)
-    .use(router) // ajouter une plugin (extension) à notre projet
+const app = createApp(App);
+
+app.directive("more" , (el, binding) => {
+    let resultat = binding.value.split(" ").slice(0, 3).join(" ")
+    if(binding.modifiers.body){
+        resultat = binding.value.split(" ").slice(0, 9).join(" ") + " ..."
+    }
+    el.innerText = resultat;
+})
+
+app.use(router) // ajouter une plugin (extension) à notre projet
+    .use(createPinia()) // comme un Provider 
     .mount('#app')
     
